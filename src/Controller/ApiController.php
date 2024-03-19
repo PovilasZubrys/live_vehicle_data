116f35ead3a321a72f92f8dde3863ec665b1c437
Povilas Zubrys <48654760+PovilasZubrys@users.noreply.github.com>
2024-03-20 00:32:26 +0200

    API endpoint for receiving data

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\ApiModel;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api/send/vehicle_data', name: 'app_send_vehicle_data', methods: 'POST')]
+    public function sendVehicleData(Request $request, ApiModel $apiModel): Response
+    {
+        $result = $apiModel->processData(json_decode($request->getContent()));
+
+        $response = new Response();
+
+        $response->setStatusCode($result['status']);
+        $response->setContent(json_encode($result));
+
+        return $response;
+    }
+}
